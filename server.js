502346cd79f91c2deef8e258e9ae8dc0b385519b
Farzanaltafbhat <farzanbhat231@gmail.com>
2024-06-05 19:54:09 +0530

    modle

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: '*', // Replace with your allowed origin
+}));
+
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
