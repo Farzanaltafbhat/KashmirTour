e6af6c6c88ad48c50e17d1863b747a622f03c7d9
Farzanaltafbhat <farzanbhat231@gmail.com>
2024-06-05 19:34:14 +0530

    files

---
+++ Models/User.js
@@ -0,0 +1,17 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  phone: String,
+  adults: Number,
+  children: Number,
+  days: Number
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
